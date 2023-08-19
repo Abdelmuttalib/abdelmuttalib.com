fa49b9ed4adaf623e5553131945b63ca0d4ea99a
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2023-08-20 00:51:32 +0800

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+
 import TailwindIndicator from "../src/components/tailwind-indicator";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+
+      <Analytics mode="production" />
       <TailwindIndicator />
     </>
   );
