69022d3cb54b58060e2cf383bd8f281216559d2b
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2024-01-22 18:35:00 +0800

    Update CVPage styling

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -17,8 +17,8 @@ export default function CVPage() {
         <meta name="description" content={PORTFOLIO_DATA.summary} />
       </Head>
 
-      <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
-        <section className="mx-auto w-full max-w-2xl space-y-8 bg-white dark:bg-transparent print:space-y-6">
+      <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16 dark:bg-background print:bg-background">
+        <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:bg-background dark:bg-transparent print:space-y-6">
           <div className="flex items-center justify-between">
             <div className="flex-1 space-y-1.5">
               <h1 className="text-2xl font-bold">{PORTFOLIO_DATA.name}</h1>
