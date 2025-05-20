02a440b8419cfb6d270ec662ca0335a9961cf5ce
Abdelmuttalib <hagar9886@gmail.com>
2025-05-20 12:02:49 +0800

    - remove unused code tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,11 +19,6 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        // globals.css
-        // --font-sans: "Inter", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
-        // Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
-        // "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-        // css variable font
         sans: "var(--font-sans)",
       },
       colors: {
